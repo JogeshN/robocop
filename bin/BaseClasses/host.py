62becfa7e90a7c15c0bd1658b297b3f70dea7943
root <pundir.ashish@live.com>
2016-11-13 08:07:52 -0800

    logging added

--- bin/BaseClasses/host.py
+++ bin/BaseClasses/host.py
@@ -1,19 +1,25 @@
 from  harness import cli
 from  robot.utils import asserts
+from  logging import debug, error, info, warn
 
 def verify_hostname(name):
 	""" verify hostname"""
 	host = cli.sshAgent()
 	print "A"
-	host.connect()
+	info('started ssh agetn %s' % host)
+	result = host.connect()
 	print "A1"
+	info('connected the ssh agent, result %s' %result)
 	hostname = host.runCmdlines('hostname')
 	print "A2"
 	print hostname
 	print name
+	info('the output of command is %s' %hostname)
 	if hostname[0].strip() == name:
 		print("passed")
+		info("Passed the testcase passed.")
 	else :
+		error("The testcase failed.")
 		asserts.fail("fail")
 
 #verify_hostname('mininet')	
--- bin/harness/cli.py
+++ bin/harness/cli.py
@@ -4,20 +4,25 @@ import os
 import paramiko
 import time
 import cmd
+from logging import debug, error , info , warn
 
 class sshAgent(cmd.Cmd):
 	def __init__(self):
 		""" intitalise the ssh Agent """
 		self.ssh= paramiko.SSHClient()
 		self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		info('The ssh agent is started !!')
 
 	def connect(self,hostname='127.0.0.1',port=22,username='mininet',password='mininet'):
 		""" take hostname , username and password as arg """
+		info('connecting to ssh agent')
 		return self.ssh.connect(hostname,port,username,password)
 
 	def runCmd(self,cmd,timeout=1):
 		""" run command and return after timeout """
-		stdin,stdout,stderr = self.ssh.exec_command("cmd")
+		info('The command %s is being executed' %cmd)
+		stdin,stdout,stderr = self.ssh.exec_command(cmd)
+		info("Sleeping for the timeout %s" %timeout)
 		time.sleep(timeout)
 		return None
 
@@ -26,12 +31,15 @@ class sshAgent(cmd.Cmd):
 		    break after if output is not there or timeout occur
 		"""
 		out = None
-		stdin,stdout,stderr = self.ssh.exec_command(cmd)	
+		info("the cmommand %s is executed on ssh expect" %cmd)
+		stdin,stdout,stderr = self.ssh.exec_command(cmd)
+		info("getting the output from ssh.")	
 		out = stdout.readlines()
 		return out
 	
 	def close():
 		""" close the ssh connection """
+		info("closing the ssh agent.")
 		self.ssh.close()
 		
 
