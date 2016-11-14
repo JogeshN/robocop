dded4a10acd6eb8776c72af4d42d3bca5ea4ef2d
root <pundir.ashish@live.com>
2016-11-14 05:25:01 -0800

    Added logging to console

--- bin/BaseClasses/host.py
+++ bin/BaseClasses/host.py
@@ -1,25 +1,29 @@
 from  harness import cli
 from  robot.utils import asserts
-from  logging import debug, error, info, warn
+import  logging 
+#ch = logging.StreamHandler()
+logger = logging.getLogger(__name__)
+#logger.addHandler(ch)
+from robot.libraries.BuiltIn import BuiltIn
+c = BuiltIn()
 
 def verify_hostname(name):
 	""" verify hostname"""
 	host = cli.sshAgent()
-	print "A"
-	info('started ssh agetn %s' % host)
+	logger.info('started ssh agetn %s' % host)
 	result = host.connect()
-	print "A1"
-	info('connected the ssh agent, result %s' %result)
+	c.log_to_console("A1")
+	logger.info('connected the ssh agent, result %s' %result)
 	hostname = host.runCmdlines('hostname')
-	print "A2"
+	c.log_to_console("A2")
 	print hostname
 	print name
-	info('the output of command is %s' %hostname)
+	logger.info('the output of command is %s' %hostname)
 	if hostname[0].strip() == name:
 		print("passed")
-		info("Passed the testcase passed.")
+		logger.info("Passed the testcase passed.")
 	else :
-		error("The testcase failed.")
+		logger.error("The testcase failed.")
 		asserts.fail("fail")
 
 #verify_hostname('mininet')	
